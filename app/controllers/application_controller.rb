22b42201061cd9f96ebe19c2fa8ff61587e7d75b
taichihub <taichikuniv21@gmail.com>
2024-08-08 21:09:50 +0900

    リフダイレクト設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    books_path
+  end
+
+  def after_sign_up_path_for(resource)
+    books_path 
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
