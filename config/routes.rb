1469f764d94f3b094b299093e114b2b03ab7fc68
taichihub <taichikuniv21@gmail.com>
2024-08-08 21:09:08 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,6 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-    
   devise_for :users
   resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  root to: 'home#index'
-
 end
