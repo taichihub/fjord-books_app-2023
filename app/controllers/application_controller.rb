7fb3455e97e15f292671e9fbdf85f4aa766fab18
taichihub <taichikuniv21@gmail.com>
2024-08-09 15:48:07 +0900

    applicationコントローラ/HTML編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     books_path
   end
 
-  def after_sign_up_path_for(resource)
-    books_path 
+  def after_sign_up_path_for(_resource)
+    books_path
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(_resource)
     new_user_session_path
   end
 end
