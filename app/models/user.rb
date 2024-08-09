9de0d9a0edb576e1d073d21c663fc704c0004f80
taichihub <taichikuniv21@gmail.com>
2024-08-09 15:46:02 +0900

    userモデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_validation :set_default_name, on: %i[create update]
+
+  validates :name, presence: true
+  validates :self_introduction, length: { maximum: 200 }
+
+  private
+
+  def set_default_name
+    self.name ||= 'BooksAppUser'
+  end
 end
