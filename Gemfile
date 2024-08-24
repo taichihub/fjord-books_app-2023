5bf6d8ca385b604e5ab35d5901b2e67ca1c6a8a6
taichihub <taichikuniv21@gmail.com>
2024-08-24 19:32:13 +0900

    0824レビュー修正

--- Gemfile
+++ Gemfile
@@ -48,7 +48,7 @@ gem 'bootsnap', require: false
 # gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
+gem 'image_processing'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
@@ -86,5 +86,4 @@ gem 'active_storage_validations'
 gem 'carrierwave'
 gem 'devise'
 gem 'devise-i18n'
-gem 'image_processing'
 gem 'kaminari'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,15 @@
 class User < ApplicationRecord
   PROFILE_IMAGE_EXTENSIONS = %w[image/jpg image/png image/gif].freeze
   MAX_PROFILE_IMAGE_SIZE = 15.megabytes
-  RESUZE_IMAGE_SIZE = [800, 800].freeze
+  RESIZE_IMAGE_SIZE = [800, 800].freeze
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_image
-  after_save :resize_large_image
-  validates :profile_image, content_type: { in: PROFILE_IMAGE_EXTENSIONS, message: I18n.t('.activerecord.errors.messages.content_type_invalid') },
-                            size: { less_than: MAX_PROFILE_IMAGE_SIZE, message: I18n.t('.activerecord.errors.messages.size_invalid') }
+  validates :profile_image, content_type: { in: PROFILE_IMAGE_EXTENSIONS },
+                            size: { less_than: MAX_PROFILE_IMAGE_SIZE }
 
-  private
-
-  def resize_large_image
-    return unless profile_image.attached? && profile_image.blob.byte_size > MAX_PROFILE_IMAGE_SIZE
-
-    resized_image = ImageProcessing::MiniMagick
-                    .source(profile_image.download)
-                    .resize_to_limit(*RESUZE_IMAGE_SIZE)
-                    .call
-    profile_image.attach(io: File.open(resized_image.path), filename: profile_image.filename, content_type: profile_image.content_type)
+  def resized_profile_image
+    profile_image.variant(resize_to_limit: RESIZE_IMAGE_SIZE).processed
   end
 end
