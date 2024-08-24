0c8ecaea4c83a9294a93d9963066a53f24a3e17e
taichihub <taichikuniv21@gmail.com>
2024-08-24 14:27:02 +0900

    books#showでコメントを取得するようにした

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,9 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1 or /books/1.json
-  def show; end
+  def show
+    @comments = @book.comments.includes(:user)
+  end
 
   # GET /books/new
   def new
