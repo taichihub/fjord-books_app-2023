4eb7a6e561ad08a93c440e4b49ce7637cfb885b6
taichihub <taichikuniv21@gmail.com>
2024-08-24 14:22:38 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
-  resources :users, only: %i(index show)
+
+  resources :books do 
+    resources :comments
+  end
+
+  resources :users do
+    resources :comments
+  end
+  
+  resources :reports do
+    resources :comments
+  end
 end
