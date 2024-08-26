4dfa2082de3097d0a6983f643a7cc47a9344d4f9
taichihub <taichikuniv21@gmail.com>
2024-08-26 17:11:18 +0900

    reportコントローラ編集

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,6 +22,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new(report_params)
 
     if @report.save
+      create_mentions(@report)
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
@@ -30,6 +31,7 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
+      create_mentions(@report)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
@@ -38,7 +40,6 @@ class ReportsController < ApplicationController
 
   def destroy
     @report.destroy
-
     redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
@@ -50,5 +51,17 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:title, :content)
+  end
+
+  def create_mentions(report)
+    report.report_mentions_as_mentioning.destroy_all
+    mentioned_report_ids = extract_mentioned_report_ids(report.content)
+    mentioned_report_ids.each do |mentioned_report_id|
+      ReportMention.create(mentioning_report_id: report.id, mentioned_report_id:)
+    end
+  end
+
+  def extract_mentioned_report_ids(content)
+    content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
   end
 end
