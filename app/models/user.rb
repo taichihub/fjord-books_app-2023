41ef2ec34caf717cafb7327d5898347e1298b86c
taichihub <taichikuniv21@gmail.com>
2024-08-31 13:07:33 +0900

    ユーザー画像のリサイズ処理をmodelからviewに記述を移行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,9 @@
 class User < ApplicationRecord
   PROFILE_IMAGE_EXTENSIONS = %w[image/jpg image/png image/gif].freeze
   MAX_PROFILE_IMAGE_SIZE = 15.megabytes
-  RESIZE_IMAGE_SIZE = [800, 800].freeze
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_image
   validates :profile_image, content_type: { in: PROFILE_IMAGE_EXTENSIONS }, size: { less_than: MAX_PROFILE_IMAGE_SIZE }
-
-  def resized_profile_image
-    profile_image.variant(resize_to_limit: RESIZE_IMAGE_SIZE).processed
-  end
 end
