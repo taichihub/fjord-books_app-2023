f5429c94b94fa95cc4033a9307a9c226847d279f
taichihub <taichikuniv21@gmail.com>
2024-09-02 19:44:03 +0900

    User・Reportモデルテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    user = User.new
+    report = Report.new(user:)
+    assert report.editable?(user)
+
+    another_user = User.new
+    assert_not report.editable?(another_user)
+  end
+
+  test '#created_on' do
+    report = Report.new(created_at: Time.zone.now)
+    assert_equal report.created_at.to_date, report.created_on
+  end
 end
